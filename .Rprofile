3bd987ab5ff271fb9f5c092742626e36c0e586be
Ben Best <ben@ecoquants.com>
2023-11-30 18:40:45 -0500

    + glossary

--- .Rprofile
+++ .Rprofile
@@ -1,1 +1,2 @@
 source("renv/activate.R")
+source("glossary/_setup.R") # glossary setup
---
+++ glossary/_setup.R
@@ -0,0 +1,19 @@
+# glossary setup - persistent across chapters
+
+# initiated by .Rprofile:
+#   source("glossary/_setup.R") # glossary setup
+
+if (!"glossary" %in% rownames(installed.packages()))
+  install.packages(
+    "glossary",
+    repos = c(
+      "https://debruine.r-universe.dev",
+      "https://cloud.r-project.org"))
+library(glossary)
+
+glossary_path("glossary/glossary.yml")
+glossary_persistent("glossary/glossary-persistent.yml")
+
+glossary_popup("click")
+
+
