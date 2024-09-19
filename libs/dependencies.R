425ed8f4cba7cf396b999cc15a17a5b5ff2ab096
Ben Best <ben@ecoquants.com>
2024-09-19 15:01:19 +0200

    + Receptors: Primary Productivity

--- libs/dependencies.R
+++ libs/dependencies.R
@@ -1,10 +1,20 @@
 # run this manually when adding R packages or Quarto extensions
 
 # add to DESCRIPTION
-pkgs <- c(
+pkgs_cran <- c(
   "dplyr", "gh", "glue", "here", "knitr", "quarto", "stringr", "tidyjson",
   "yaml")
-sapply(pkgs, usethis::use_package)
+pkgs_github <- c(
+  "offshorewindhabitat/offhabr")
+
+sapply(pkgs_cran, usethis::use_package)
+#sapply(pkgs_github, usethis::use_dev_package)
+# TODO: function to install from GitHub using basename
+# use_github <- function(pkg){
+#   usethis::use_dev_package(pkg, remote = paste0("offshorewindhabitat/", pkg))
+# }
+
+usethis::use_dev_package("offhabr", remote = "offshorewindhabitat/offhabr")
 
 # install Quarto _extensions in Terminal:
 #  quarto add quarto-ext/fontawesome
