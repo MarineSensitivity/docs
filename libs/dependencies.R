adf77d7e48a5278ceb7a0632f214f8ebfc32488d
Ben Best <ben@ecoquants.com>
2024-09-20 11:25:10 +0200

    vgpm swap code (offhabr install off) w/ img

--- libs/dependencies.R
+++ libs/dependencies.R
@@ -4,17 +4,19 @@
 pkgs_cran <- c(
   "dplyr", "gh", "glue", "here", "knitr", "quarto", "stringr", "tidyjson",
   "yaml")
-pkgs_github <- c(
-  "offshorewindhabitat/offhabr")
+# pkgs_github <- c(
+#   "offshorewindhabitat/offhabr")
 
 sapply(pkgs_cran, usethis::use_package)
+
 #sapply(pkgs_github, usethis::use_dev_package)
 # TODO: function to install from GitHub using basename
 # use_github <- function(pkg){
 #   usethis::use_dev_package(pkg, remote = paste0("offshorewindhabitat/", pkg))
 # }
-
-usethis::use_dev_package("offhabr", remote = "offshorewindhabitat/offhabr")
+#
+# GH Action problem with offhabr install https://github.com/MarineSensitivity/docs/actions/runs/10956584027/job/30422934639
+# usethis::use_dev_package("offhabr", remote = "offshorewindhabitat/offhabr")
 
 # install Quarto _extensions in Terminal:
 #  quarto add quarto-ext/fontawesome
